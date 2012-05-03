eb8e0ec389300cab49bf48fce7b13ca1b8e1918f
Travis Lewis <econobeing5@gmail.com>
2012-05-02 22:27:46 -0700

    okay, last commit for the night...

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,7 +6,12 @@ public class MainClass
 	 */
 	public static void main(String[] args)
 	{
-		EasyChallenges.FizzBuzz.showNum(20);
+		//EasyChallenges.FizzBuzz.showNum(20);
+		System.out.println(Character.getNumericValue('a'));
+		System.out.println(Character.getNumericValue('A'));
+		System.out.println(Character.getNumericValue('z'));
+		System.out.println(Character.getNumericValue('/'));
+		
 	}
 
 }
