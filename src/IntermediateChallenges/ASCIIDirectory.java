37c67d464c54e3e25a0a4a7c96abe1f48370337e
Travis Lewis <econobeing5@gmail.com>
2012-05-10 23:58:02 -0700

    finished challenge #50: ASCII directory tree printing

---
+++ src/IntermediateChallenges/ASCIIDirectory.java
@@ -0,0 +1,36 @@
+package IntermediateChallenges;
+
+import java.io.File;
+
+/**
+ * @author Travis Lewis
+ * Date: 13 April 2012
+ * Challenge #50
+ * 
+ * Given an absolute path, write a program that outputs an ASCII tree of 
+ * that directory.
+ * 
+ * Note: 'tree' utility is not allowed.
+ * Extra credit: Limit the depth of the tree by variable n.
+ */
+public class ASCIIDirectory 
+{
+    public static String printDirectory(final String path, final int depth) {
+        String result = "";
+        File[] list = (new File(path)).listFiles();
+        
+        for(int i = 0 ; i < list.length ; i++){
+            for(int j = 0 ; j < depth ; j++)
+                result += "    ";
+            result += "+-";
+            
+            if(list[i].isDirectory())
+                result += list[i].getName() + "/\n" +
+                        printDirectory(list[i].getAbsolutePath(), depth+1);
+            else
+                result += list[i].getName() + "\n";
+        }
+        return result;
+    }
+    
+}
